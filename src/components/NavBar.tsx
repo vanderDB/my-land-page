3b261556a0ee0aa80c470ea6a8d972c48a517517
vanderZet-dev <85804034+vanderDB@users.noreply.github.com>
2021-07-26 23:35:06 +0300

    refactor nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,14 +4,49 @@ import {NavBarElem} from "./NavBarElem";
 
 export function NavBar() {
 
+    const navigations = [
+        {
+            id: 1,
+            label: 'Home',
+            path: 'home',
+            sort: 1,
+            enabled: true,
+        },
+        {
+            id: 2,
+            label: 'Services',
+            path: 'services',
+            sort: 2,
+            enabled: true,
+        },
+        {
+            id: 3,
+            label: 'Works',
+            path: 'works',
+            sort: 3,
+            enabled: true,
+        },
+        {
+            id: 4,
+            label: 'Team',
+            path: 'team',
+            sort: 4,
+            enabled: true,
+        },
+        {
+            id: 5,
+            label: 'Contact',
+            path: 'contact',
+            sort: 5,
+            enabled: true,
+        },
+    ];
 
     return (
         <StyledNavBar>
-            <NavBarElem label='Home' path='home'/>
-            <NavBarElem label='Services' path='services'/>
-            <NavBarElem label='Works' path='works'/>
-            <NavBarElem label='Team' path='team'/>
-            <NavBarElem label='Contact' path='contact'/>
+            {navigations.filter(el => el.enabled).sort((a, b) => a.sort > b.sort ? 1 : -1).map(nav => (
+                <NavBarElem key={nav.id} label={nav.label} path={nav.path}/>
+            ))}
         </StyledNavBar>
     );
 }

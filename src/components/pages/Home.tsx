aa74d0fda1f73904b378d6c6307e80042a3330ed
vanderZet-dev <85804034+vanderDB@users.noreply.github.com>
2021-07-31 00:41:28 +0300

    add home banner

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,7 +1,40 @@
+import {HomeBanner} from "../../assets/img";
+import styled from "styled-components";
+
 export function Home() {
     return (
         <div>
-            Home
+            <StyledBannerContainer>
+                <StyledHomeBannerTitle>
+                    We help your business grow
+                </StyledHomeBannerTitle>
+                <StyledImageContainer>
+                    <img src={HomeBanner}/>
+                </StyledImageContainer>
+            </StyledBannerContainer>
         </div>
     );
 };
+
+const StyledBannerContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  margin-top: 100px;
+`;
+
+const StyledHomeBannerTitle = styled.div`  
+  font-size: 40px;
+
+  max-width: 350px;
+  margin-right: 10px;
+`;
+
+const StyledImageContainer = styled.div`
+  width: 500px;
+  
+  img {
+    width: 100%;
+  }
+`;

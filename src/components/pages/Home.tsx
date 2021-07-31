a771f5f42a8990b9e541c719192bca12fb3c6cb3
vanderZet-dev <85804034+vanderDB@users.noreply.github.com>
2021-07-31 21:09:47 +0300

    refactor

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,14 +1,12 @@
-import {HomeBannerImg} from "../../assets/img";
-import styled from "styled-components";
 import HomeBanner from "./home/HomeBanner";
 import MessageToClient from "./home/MessageToClient";
 
 export function Home() {
     return (
         <div>
-            <HomeBanner />
-            <MessageToClient />
+            <HomeBanner/>
+            <MessageToClient/>
         </div>
     );
-};
+}
 
